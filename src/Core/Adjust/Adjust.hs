11d459f1244ab8920db1324d1b20ca29f97be91c
Paulo Cavalcanti <paulojcvf@gmail.com>
2025-08-29 13:57:25 +0400

    WIP: bring new reduceEtas

--- src/Core/Adjust/Adjust.hs
+++ src/Core/Adjust/Adjust.hs
@@ -84,14 +84,17 @@ import Core.WHNF
 -- book adjustment where recursive references aren't available yet.
 adjust :: Book -> Term -> Term
 adjust book term =
+  -- trace ("nfrk: " ++ show nfrk) $
   -- trace ("done: " ++ show done) $
   done
   where
     flat = flattenPats 0 noSpan book term
     npat = desugarPats 0 noSpan flat
     nfrk = desugarFrks book 0 npat
-    etas = reduceEtas 0 nfrk
-    done = bind etas
+    hoas = bind nfrk
+    done = reduceEtas 0 hoas
+    -- etas = reduceEtas 0 nfrk
+    -- done = bind etas
 
 -- | Adjusts a term. simplifying patterns but leaving terms as Pats.
 adjustWithPats :: Book -> Term -> Term
